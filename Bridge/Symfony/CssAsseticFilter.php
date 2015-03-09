b37b323eead247bb9f19a858c6b4d3eda7b8bde4
ihsanudin <surya.kejawen@gmail.com>
2015-03-09 16:56:09 +0700

    add assetic filter

---
+++ Bridge/Symfony/CssAsseticFilter.php
@@ -0,0 +1,26 @@
+<?php
+namespace Ihsan\Compressor\Bridge\Symfony;
+
+/**
+ * Author: Muhammad Surya Ihsanuddin<surya.kejawen@gmail.com>
+ * Url: http://blog.khodam.org
+ */
+
+use Assetic\Filter\FilterInterface;
+use Assetic\Asset\AssetInterface;
+use Ihsan\Compressor\CssCompressor;
+
+class CssAsseticFilter implements FilterInterface
+{
+    public function filterDump(AssetInterface $asset)
+    {
+        $css = new CssCompressor($asset->getContent());
+
+        $asset->setContent($css->compress());
+    }
+
+    public function filterLoad(AssetInterface $asset)
+    {
+        $this->filterDump($asset);
+    }
+}
